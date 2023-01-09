ed36f514202e53a6f3d659d25a45face7707e324
miracle3070 <miracle3070@naver.com>
2023-01-09 21:41:22 +0900

    Hello JPA - 애플리케이션 개발 (JPQL 약간)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,11 +15,16 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setId(2L);
-            member.setName("HelloB");
+//            Member member = new Member();
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
 
-            em.persist(member);
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
